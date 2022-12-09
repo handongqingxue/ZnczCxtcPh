d3c4e58801da284a514277dd44553fc0cef9b84f
handongqingxue <1417140290@qq.com>
2022-12-09 16:58:56 +0800

    完善

---
+++ pages/home/home.js
@@ -0,0 +1,81 @@
+// pages/home/home.js
+var home;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ddgl_ddzt:1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    home=this;
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  goPage:function(e){
+    let pageFlag=e.currentTarget.dataset.pageflag;
+    let url="/pages/";
+    console.log(pageFlag)
+    switch (pageFlag) {
+      case home.data.ddgl_ddzt:
+        url+='ddgl/ddzt/list';
+        break;
+    }
+    wx.redirectTo({
+      url: url,
+    })
+  }
+})
