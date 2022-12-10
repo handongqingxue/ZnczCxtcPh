1ac8c55826f7f7c012d4531844b8dcfa3543da4b
handongqingxue <1417140290@qq.com>
2022-12-10 14:11:08 +0800

    完善

--- pages/ddgl/ddzt/list.js
+++ pages/ddgl/ddzt/list.js
@@ -1,5 +1,6 @@
 // pages/ddgl/ddzt/ddzt.js
-var ddztList;
+var ddztListPage;
+var rootIP;
 Page({
 
   /**
@@ -9,14 +10,19 @@ Page({
     backButSign:'<',
     showDjckgdView:true,
     showPageView:false,
-    showToolBarView:true
+    showToolBarView:true,
+    currentPage:1,
+    pageSize:10,
+    showNoDataView:false,
+    mc:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    ddztList=this;
+    ddztListPage=this;
+    rootIP=getApp().getRootIP();
   },
 
   /**
@@ -69,19 +75,61 @@ Page({
   },
   showPageView:function(flag){
     if(flag){
-      ddztList.setData({showDjckgdView:false,showPageView:true});
+      ddztListPage.setData({showDjckgdView:false,showPageView:true});
     }
     else{
-      ddztList.setData({showDjckgdView:true,showPageView:false});
+      ddztListPage.setData({showDjckgdView:true,showPageView:false});
     }
   },
   showToolBarView:function(e){
     let flag=e.currentTarget.dataset.flag;
     if(flag=="true"){
-      ddztList.setData({showToolBarView:true});
+      ddztListPage.setData({showToolBarView:true});
     }
     else if(flag=="false"){
-      ddztList.setData({showToolBarView:false});
+      ddztListPage.setData({showToolBarView:false});
     }
+  },
+  showNoDataView:function(flag){
+    if(flag=="true"){
+      ddztListPage.setData({showNoDataView:true});
+    }
+    else if(flag=="false"){
+      ddztListPage.setData({showNoDataView:false});
+    }
+  },
+  getInputValue:function(e){
+    if(e.currentTarget.id=="mc_inp"){
+      let mc=e.detail.value;
+      ddztListPage.setData({mc:mc});
+    }
+  },
+  selectListData:function(){
+    let currentPage=ddztListPage.data.currentPage;
+    let pageSize=ddztListPage.data.pageSize;
+    let mc=ddztListPage.data.mc;
+    wx.request({
+      url: rootIP+"queryDDZTList",
+      data:{page:currentPage,rows:pageSize,mc:mc},
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function (res) {
+        let data=res.data;
+        let status=data.status;
+        console.log("status==="+status)
+        if(status=="ok"){
+          var ddztList=data.list;
+          ddztListPage.setData({ddztList:ddztList});
+        }
+        else{
+          ddztListPage.showNoDataView(true);
+          ddztListPage.setData({noDataText:data.message});
+        }
+        let e={currentTarget:{dataset:{flag:"false"}}};
+        ddztListPage.showToolBarView(e);
+      }
+    })
   }
 })
