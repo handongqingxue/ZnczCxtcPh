e5774a355bb03e58f731c66e6fcebb0202b16105
handongqingxue <1417140290@qq.com>
2022-12-22 15:55:07 +0800

    完善

--- pages/sjgl/dsh/detail.js
+++ pages/sjgl/dsh/detail.js
@@ -1,25 +1,34 @@
 // pages/sjgl/dsh/detail.js
+var detailPage;
+var rootIP;
+var serverRootIP;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    backButSign:'<',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    detailPage=this;
+    rootIP=getApp().getRootIP();
+    serverRootIP=getApp().getServerRootIP();
+    //let id=options.id;
+    let id=4;
+    console.log(id);
+    detailPage.setData({id:id});
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    detailPage.getSJInfo();
   },
 
   /**
@@ -62,5 +71,35 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  getSJInfo:function(){
+    let id=detailPage.data.id;
+    wx.request({
+      url: rootIP+"getSiJi",
+      method: 'POST',
+      data: { id:id},
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function (res) {
+        console.log(res);
+        let data=res.data;
+        let sj=data.sj;
+        let xm=sj.xm;
+        let sjh=sj.sjh;
+        let sfzzp=sj.sfzzp;
+        let sfzh=sj.sfzh;
+        let zgzyxqz=sj.zgzyxqz;
+        let jzyxqz=sj.jzyxqz;
+        let zgzs=sj.zgzs;
+        let jz=sj.jz;
+        detailPage.setData({xm:xm,sjh:sjh,sfzzp:sfzzp==null?null:serverRootIP+sfzzp,sfzh:sfzh,zgzyxqz:zgzyxqz,jzyxqz:jzyxqz,zgzs:zgzs==null?null:serverRootIP+zgzs,jz:jz==null?null:serverRootIP+jz});
+      }
+    })
+  },
+  goListPage:function(){
+    wx.redirectTo({
+      url: '/pages/sjgl/dsh/list',
+    })
+  },
 })
