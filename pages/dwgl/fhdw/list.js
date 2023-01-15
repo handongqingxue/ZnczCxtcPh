a77262eaf77f89b0cb127ca74d67b892be5d23b4
handongqingxue <1417140290@qq.com>
2023-01-15 10:18:18 +0800

    完善

---
+++ pages/dwgl/fhdw/list.js
@@ -0,0 +1,192 @@
+// pages/dwgl/fhdw/list.js
+var fhdwListPage;
+var rootIP;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    backButSign:'<',
+    showDjckgdView:true,
+    showPageView:false,
+    showToolBarView:false,
+    currentPage:1,
+    pageSize:10,
+    showNoDataView:false,
+    mc:"",
+    prePageFlag:1,
+    nextPageFlag:2,
+    prePageEnable:false,
+    nextPageEnable:true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    fhdwListPage=this;
+    rootIP=getApp().getRootIP();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    fhdwListPage.getListData();
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  showPageView:function(flag){
+    if(flag){
+      fhdwListPage.setData({showDjckgdView:false,showPageView:true});
+    }
+    else{
+      fhdwListPage.setData({showDjckgdView:true,showPageView:false});
+    }
+  },
+  showToolBarView:function(e){
+    let flag=e.currentTarget.dataset.flag;
+    if(flag){
+      fhdwListPage.setData({showToolBarView:true});
+    }
+    else{
+      fhdwListPage.setData({showToolBarView:false});
+    }
+  },
+  showNoDataView:function(flag){
+    if(flag){
+      fhdwListPage.setData({showNoDataView:true});
+    }
+    else{
+      fhdwListPage.setData({showNoDataView:false});
+    }
+  },
+  getInputValue:function(e){
+    if(e.currentTarget.id=="mc_inp"){
+      let mc=e.detail.value;
+      fhdwListPage.setData({mc:mc});
+    }
+  },
+  loadListDataByPageFlag:function(e){
+    let flag=e.currentTarget.dataset.flag;
+    let prePageFlag=fhdwListPage.data.prePageFlag;
+    let nextPageFlag=fhdwListPage.data.nextPageFlag;
+    let currentPage=fhdwListPage.data.currentPage;
+    let pageCount=fhdwListPage.data.pageCount;
+    if(flag==prePageFlag)
+      currentPage--;
+    else if(flag==nextPageFlag)
+      currentPage++;
+
+    if(currentPage<1)
+      currentPage=1;
+    else if(currentPage>pageCount)
+      currentPage=pageCount;
+
+    if(currentPage<=1){
+      fhdwListPage.setData({prePageEnable:false,nextPageEnable:true});
+    }
+    else if(currentPage>=pageCount){
+      fhdwListPage.setData({prePageEnable:true,nextPageEnable:false});
+    }
+    fhdwListPage.setData({currentPage:currentPage});
+    fhdwListPage.getListData();
+  },
+  getListData:function(){
+    let currentPage=fhdwListPage.data.currentPage;
+    let pageSize=fhdwListPage.data.pageSize;
+    let mc=fhdwListPage.data.mc;
+    wx.request({
+      url: rootIP+"getFaHuoDanWeiList",
+      data:{page:currentPage,rows:pageSize,mc:mc},
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function (res) {
+        let data=res.data;
+        let status=data.status;
+        console.log("status==="+status)
+        let dataCount;
+        fhdwListPage.setData({fhdwList:[]});
+        if(status=="ok"){
+          var fhdwList=data.list;
+          fhdwListPage.setData({fhdwList:fhdwList});
+          fhdwListPage.showNoDataView(false);
+          fhdwListPage.setData({noDataText:""});
+        }
+        else{
+          fhdwListPage.showNoDataView(true);
+          fhdwListPage.setData({noDataText:data.message});
+        }
+        dataCount=data.total;
+        fhdwListPage.setData({dataCount:dataCount,pageCount:Math.floor((dataCount-1)/pageSize)+1});
+        let e={currentTarget:{dataset:{flag:false}}};
+        fhdwListPage.showToolBarView(e);
+      }
+    })
+  },
+  goAddPage:function(){
+    wx.redirectTo({
+      url: '/pages/dwgl/fhdw/new',
+    })
+  },
+  goEditPage:function(e){
+    let id=e.currentTarget.dataset.id;
+    wx.redirectTo({
+      url: '/pages/dwgl/fhdw/edit?id='+id,
+    })
+  },
+  goDetailPage:function(e){
+    let id=e.currentTarget.dataset.id;
+    wx.redirectTo({
+      url: '/pages/dwgl/fhdw/detail?id='+id,
+    })
+  },
+  goHomePage:function(){
+    wx.redirectTo({
+      url: '/pages/home/home',
+    })
+  }
+})
