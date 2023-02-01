61202cdb81d1bca54280d8e3ec0106cb05fec24e
handongqingxue <1417140290@qq.com>
2023-02-01 11:08:33 +0800

    完善

--- pages/ddgl/dzj/qrcodeInfo.js
+++ pages/ddgl/dzj/qrcodeInfo.js
@@ -79,8 +79,10 @@ Page({
       success: function (res) {
         let constantFlagMap=res.data;
         console.log(constantFlagMap);
+        let lxlx=constantFlagMap.lxlx;
         let ddzt=constantFlagMap.ddzt;
-        let constantFlags=ddzt;
+        let ddShlx=constantFlagMap.ddShlx;
+        let constantFlags=lxlx+","+ddzt+","+ddShlx;
         qrcodeInfoPage.getConstantMap(constantFlags);
       }
     })
@@ -115,10 +117,88 @@ Page({
         console.log(res);
         let data=res.data;
         let dingDan=data.dingDan;
+        let id=dingDan.id;
         let ddh=dingDan.ddh;
         let cyclCph=dingDan.cyclCph;
-        qrcodeInfoPage.setData({ddh:ddh,cyclCph:cyclCph});
+        let wzMc=dingDan.wzMc;
+        let yssMc=dingDan.yssMc;
+        let fhdwMc=dingDan.fhdwMc;
+        let shdwMc=dingDan.shdwMc;
+        let lxlx=dingDan.lxlx;
+        let lxlxMc=qrcodeInfoPage.getLxlxMcById(lxlx);
+        let jhysrq=dingDan.jhysrq;
+        let yzxzl=dingDan.yzxzl;
+        qrcodeInfoPage.setData({id:id,ddh:ddh,cyclCph:cyclCph,wzMc:wzMc,yssMc:yssMc,fhdwMc:fhdwMc,shdwMc:shdwMc,lxlxMc:lxlxMc,jhysrq:jhysrq,yzxzl:yzxzl});
       }
     })
   },
+  getLxlxMcById:function(lxlxId){
+    let constantMap=qrcodeInfoPage.data.constantMap;
+    let lxlxMap=constantMap.lxlxMap;
+    //console.log(lxlxMap);
+    var str;
+    switch (lxlxId) {
+      case lxlxMap.syLxlx:
+        str=lxlxMap.syLxlxMc;//送运
+        break;
+      case lxlxMap.qyLxlx:
+        str=lxlxMap.qyLxlxMc;//取运
+        break;
+    }
+    return str;
+  },
+  checkById:function(e){
+    let confirmStr="请确保所有订单都认真审核过！";
+    wx.showModal({
+      title: "提示",
+      content: confirmStr,
+      success (res) {
+        if (res.confirm) {
+          //console.log('用户点击确定')
+          let id=qrcodeInfoPage.data.id;
+          let shjg=e.currentTarget.dataset.shjg;
+          let ddztConstantMap=qrcodeInfoPage.data.constantMap.ddztMap;
+          let yjdsmDdztMc=ddztConstantMap.yjdsmDdztMc;
+          let ddztMc;
+          if(shjg)
+            ddztMc=yjdsmDdztMc;
+          let ddShlxConstantMap=qrcodeInfoPage.data.constantMap.ddShlxMap;
+          let shlx=ddShlxConstantMap.zjshShlx;
+          let yongHu=wx.getStorageSync("yongHu");
+          let shrId=yongHu.id;
+          wx.request({
+            url: rootIP+"checkDingDanByIds",
+            data:{ids:id,ddztMc:ddztMc,shlx:shlx,shjg:shjg,shrId:shrId},
+            method: 'POST',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded',
+            },
+            success: function (res) {
+              let data=res.data;
+              let status=data.status;
+              console.log("status==="+status)
+              if(status==1){
+                wx.showToast({
+                  title: data.msg,
+                })
+                qrcodeInfoPage.goListPage();
+              }
+              else{
+                wx.showToast({
+                  title: data.msg,
+                })
+              }
+            }
+          })
+        } else if (res.cancel) {
+          //console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  goListPage:function(){
+    wx.redirectTo({
+      url: '/pages/ddgl/dzj/list',
+    })
+  }
 })
